c865104b77c1968cf51a6fe330c83a2330d3ea14
nima abdpour <nimaabdpoor@gmail.com>
2022-02-06 21:58:30 +0330

    starting spring Tutorial project.

---
+++ src/test/kotlin/com/nima/springboottutorial/SpringBootTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nima.springboottutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
