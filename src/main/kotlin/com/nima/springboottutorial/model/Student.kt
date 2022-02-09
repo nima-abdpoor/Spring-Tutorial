1590a9207f409f2fa6647e5f73f3b1aadc8c8218
nima abdpour <nimaabdpoor@gmail.com>
2022-02-09 21:25:46 +0330

    student class created.

---
+++ src/main/kotlin/com/nima/springboottutorial/model/Student.kt
@@ -0,0 +1,15 @@
+package com.nima.springboottutorial.model
+
+import java.time.LocalDate
+
+data class Student(
+    var id: Long? = null,
+    var name: String,
+    var age: Int,
+    var dob: LocalDate,
+    var email: String
+) {
+    override fun toString(): String {
+        return "Student(id=$id, name='$name', age=$age, dob=$dob, email='$email')"
+    }
+}
