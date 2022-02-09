47ed01144ee5d9df0f9e0fc3b92c89f65bf029fc
nima abdpour <nimaabdpoor@gmail.com>
2022-02-09 21:26:14 +0330

    student builder class that generates custom number of students.

---
+++ src/main/kotlin/com/nima/springboottutorial/StudentBuilder.kt
@@ -0,0 +1,29 @@
+package com.nima.springboottutorial
+
+import com.nima.springboottutorial.model.Student
+import java.time.LocalDate
+import java.time.Month
+
+class StudentBuilder {
+    private val result = ArrayList<Student>()
+    fun build(
+        number: Int = 5,
+        id: Long? = null,
+        name: String = "Nima",
+        email: String = "NimaAbdpoor@gmail.com",
+        age: Int = 20,
+        date: LocalDate = LocalDate.of(2022, Month.APRIL, 28)
+    ): List<Student> {
+        for (x in 0..number)
+            result.add(
+                Student(
+                    id = id,
+                    name = name + if (x > 0) x.toString() else "",
+                    email = email + if (x > 0) x.toString() else "",
+                    age = age + x,
+                    dob = date
+                )
+            )
+        return result
+    }
+}
