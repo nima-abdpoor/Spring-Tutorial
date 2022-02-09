38a1cb6577931acd02a15f938a1e3ee5129b96c5
nima abdpour <nimaabdpoor@gmail.com>
2022-02-09 21:26:41 +0330

    get method that gets list of students.

--- src/main/kotlin/com/nima/springboottutorial/SpringBootTutorialApplication.kt
+++ src/main/kotlin/com/nima/springboottutorial/SpringBootTutorialApplication.kt
@@ -1,5 +1,6 @@
 package com.nima.springboottutorial
 
+import com.nima.springboottutorial.model.Student
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +13,11 @@ class SpringBootTutorialApplication {
     @GetMapping("/hello")
     fun sayHello(@RequestParam(value = "myName", defaultValue = "World") name: String?): String {
         return String.format("Hello %s!", name)
+    }
+
+    @GetMapping("/student")
+    fun getStudent(): List<Student> {
+        return StudentBuilder().build(number = 10)
     }
 
     companion object {
