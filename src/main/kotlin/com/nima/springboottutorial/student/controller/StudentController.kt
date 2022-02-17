07cd5e5b5e7b9da2682845a6321c2957498de4c5
nima abdpour <nimaabdpoor@gmail.com>
2022-02-17 09:19:14 +0330

    get custom number of students from input.

--- src/main/kotlin/com/nima/springboottutorial/student/controller/StudentController.kt
+++ src/main/kotlin/com/nima/springboottutorial/student/controller/StudentController.kt
@@ -4,13 +4,14 @@ import com.nima.springboottutorial.StudentBuilder
 import com.nima.springboottutorial.student.model.Student
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/v1/student")
 class StudentController {
     @GetMapping
-    fun getStudent(): List<Student> {
-        return StudentBuilder().build(number = 10)
+    fun getStudent(@RequestParam(value = "number", defaultValue = "5") number: String): List<Student> {
+        return StudentBuilder().build(number = number.toInt())
     }
 }
