49c4cc5cd33897cb9b7facffe3c2fb7dc340ded2
nima abdpour <nimaabdpoor@gmail.com>
2022-02-17 09:36:03 +0330

    using DI for injecting service class into controller.

--- src/main/kotlin/com/nima/springboottutorial/student/controller/StudentController.kt
+++ src/main/kotlin/com/nima/springboottutorial/student/controller/StudentController.kt
@@ -1,7 +1,8 @@
 package com.nima.springboottutorial.student.controller
 
-import com.nima.springboottutorial.StudentBuilder
 import com.nima.springboottutorial.student.model.Student
+import com.nima.springboottutorial.student.service.StudentService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/v1/student")
-class StudentController {
+class StudentController(@Autowired private val studentService: StudentService) {
+
     @GetMapping
     fun getStudent(@RequestParam(value = "number", defaultValue = "5") number: String): List<Student> {
-        return StudentBuilder().build(number = number.toInt())
+        return studentService.getStudent(number)
     }
 }
